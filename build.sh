503e3b16c60e2f9dc696ef4f71222ab437198feb
Karlo Tablang <karlo@tabla.ng>
2023-10-14 04:23:51 +0800

    Fix build script

--- build.sh
+++ build.sh
@@ -4,17 +4,12 @@ if [ "$CF_PAGES_BRANCH" == "production" ]; then
   # Run the "production" script in `package.json` on the "production" branch
   # "production" should be replaced with the name of your Production branch
   # npm run production
-  pip install -r requirements
+  pip install -r requirements.txt
   python main.py
-
 elif [ "$CF_PAGES_BRANCH" == "staging" ]; then
   # Run the "staging" script in `package.json` on the "staging" branch
   # "staging" should be replaced with the name of your specific branch
   # npm run staging
-  pip install -r requirements
+  pip install -r requirements.txt
   python main.py
-
-else
-  # Else run the dev script
-  # npm run dev
 fi
